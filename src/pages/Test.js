e8a261ba07872ea1a66b32fbfb684bb576f1b121
Ratnesh <ratneshpandey965@gmail.com>
2021-12-09 19:17:05 +0530

    Login image hide

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -57,7 +57,7 @@ export default function Test() {
     
     <div className="col-sm-6 text-center">
     <img src={Wework} className="img-fluid wework"/>
-     <div className="ml-5 text-left"><img src={Loginbtn} onClick={()=>setLoginFlag(!loginFlag)} className="img-fluid loginbtn pl-5"/> </div> 
+     {/* <div className="ml-5 text-left"><img src={Loginbtn} onClick={()=>setLoginFlag(!loginFlag)} className="img-fluid loginbtn pl-5"/> </div>  */}
      </div>
     <div className="col-sm-6 text-center"><img src={Wenot} className="img-fluid wenot"  />
     <br/><img src={datetime}  className="img-fluid wenot mt-lg-5 mt-md-3 mt-sm-3"/>
